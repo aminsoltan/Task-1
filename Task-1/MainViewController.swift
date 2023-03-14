8cbd3c19f84222649a0bd507a721751d32c6cc96
aminsoltan <aminsoltani1881@gmail.com>
2023-03-14 21:55:36 +0330

    main tableView added

--- Task-1/MainViewController.swift
+++ Task-1/MainViewController.swift
@@ -1,22 +1,55 @@
-//
-//  ViewController.swift
-//  Task-1
-//
-//  Created by seyedali hamedi on 3/14/23.
-//
-
 import UIKit
 
 class MainViewController: UIViewController  {
-
+    
+    let strArray: [String] = ["baboone","lavender","sarakhs","kaaj","rose","tomato"]
+    
+    private let mainTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        return table
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor =  .systemBackground
         
-      
+        view.backgroundColor =  .green
+        view.addSubview(mainTable)
+        mainTable.dataSource = self
+        mainTable.delegate = self
+        
+        
+        
     }
-
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mainTable.frame=view.bounds
+    }
+    
+    
+    
 }
+
+extension MainViewController: UITableViewDelegate , UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return strArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = strArray[indexPath.item]
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+}
+    
+    
+
+    
 
