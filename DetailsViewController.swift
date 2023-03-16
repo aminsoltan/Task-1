63e5a1e124204d9caf97d716d81de54412148f4d
Ali Hamedi <hamediseyedali@gmail.com>
2023-03-16 14:08:08 +0330

    finishing navigation system and dynamic ui

---
+++ DetailsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailsViewController.swift
+//  Task-1
+//
+//  Created by seyedali hamedi on 3/16/23.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    var stringPassed = ""
+    let descriptionDic : [String:String] = [
+        "baboone":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "lavender":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "sarakhs":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "kaaj":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "rose":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "tomato":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    ]
+    
+    private let mainLabel : UILabel = {
+        let title = UILabel()
+        title.font = .systemFont(ofSize: 52,weight: .semibold)
+        title.textColor = .white
+        title.lineBreakMode = .byWordWrapping
+        title.numberOfLines = 0
+        title.frame = CGRect(x: 100, y: 150, width: 200, height: 100)
+        return title
+    }()
+    
+    private let decriptionLabel : UILabel = {
+        let title = UILabel()
+        title.font = .systemFont(ofSize: 24,weight: .semibold)
+        title.textColor = UIColor.white.withAlphaComponent(0.9)
+        title.lineBreakMode = .byCharWrapping
+        title.numberOfLines = 0
+        title.frame = CGRect(x:100, y:300, width: 200, height: 200)
+        return title
+    }()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        
+        mainLabel.text = stringPassed
+        view.addSubview(mainLabel)
+        decriptionLabel.text = descriptionDic[stringPassed]
+        view.addSubview(decriptionLabel)
+        
+       
+    }
+
+
+
+}
--- Task-1/MainViewController.swift
+++ Task-1/MainViewController.swift
@@ -1,23 +1,32 @@
+
 import UIKit
 
 class MainViewController: UIViewController  {
-    
+
     let strArray: [String] = ["baboone","lavender","sarakhs","kaaj","rose","tomato"]
+   
+
     
-    private let mainTable: UITableView = {
-        let table = UITableView(frame: .zero, style: .grouped)
+    private let mainTable : UITableView =  {
+        let table = UITableView(frame: .zero,style:.grouped)
         table.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
         return table
     }()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor =  .green
+  
+        
         view.addSubview(mainTable)
-        mainTable.dataSource = self
+        
+        
         mainTable.delegate = self
+        mainTable.dataSource = self
         
         
+   
         
     }
     
@@ -25,31 +34,41 @@ class MainViewController: UIViewController  {
         super.viewDidLayoutSubviews()
         mainTable.frame=view.bounds
     }
-    
-    
-    
+
+
+
 }
 
-extension MainViewController: UITableViewDelegate , UITableViewDataSource {
+extension MainViewController : UITableViewDelegate,UITableViewDataSource{
+
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return strArray.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
         cell.textLabel?.text = strArray[indexPath.item]
-        cell.backgroundColor = .systemGreen
+        cell.backgroundColor = .green
+       
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
-}
-    
     
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let navigationController = navigationController{
+            let viewController = DetailsViewController()
+            viewController.stringPassed = strArray[indexPath.item]
+            navigationController.pushViewController(viewController, animated: true)
+        }
+    }
     
+}
 
